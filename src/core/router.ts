8575f7a943c46565bdf29f4f20aecb5b6221e648
Gifaldy Azka <me@falcxxdev.ml>
2022-06-28 18:31:09 +0700

    feat: update document meta on route change (#7)
    * feat: update document meta on route change
    Document will update title & meta tag on route change. As it defined inside meta object
    * feat: specify datatype for meta object
    Specify data types for meta object. And it's optional (?:)
    * chore: update meta description tag & og:site_name

--- src/core/router.ts
+++ src/core/router.ts
@@ -13,26 +13,31 @@ const routes: Routes = [
         path: '/',
         name: 'Home',
         component: Home,
+        meta: { title: 'Home | Gifaldy Azka' },
     },
     {
         path: '/about',
         name: 'About',
         component: About,
+        meta: { title: 'About | Gifaldy Azka' },
     },
     {
         path: '/projects',
         name: 'Projects',
         component: Projects,
+        meta: { title: 'Projects | Gifaldy Azka' },
     },
     {
         path: '/contact',
         name: 'Contact',
         component: Contact,
+        meta: { title: 'Contact | Gifaldy Azka' },
     },
     {
         path: '/:catchAll(.*)',
         name: 'NotFound',
         component: NotFound,
+        meta: { title: 'Page not Found | Gifaldy Azka' },
     },
 ];
 
@@ -42,9 +47,36 @@ const router: Router = createRouter({
 });
 
 router.beforeResolve((to, from, next) => {
-    if (to.name) {
-        NProgress.start();
+    if (to.name) NProgress.start();
+    const nearestWithTitle: any = to.matched
+        .slice()
+        .reverse()
+        .find(r => r.meta && r.meta.title);
+    const nearestWithMeta: any = to.matched
+        .slice()
+        .reverse()
+        .find(r => r.meta && r.meta.metaTags);
+    const previousNearestWithMeta: any = from.matched
+        .slice()
+        .reverse()
+        .find(r => r.meta && r.meta.metaTags);
+    if (nearestWithTitle) {
+        document.title = nearestWithTitle.meta.title;
+    } else if (previousNearestWithMeta) {
+        document.title = previousNearestWithMeta.meta.title;
     }
+    Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
+    if (!nearestWithMeta) return next();
+    nearestWithMeta.meta.metaTags
+        .map(tagDef => {
+            const tag: HTMLMetaElement = document.createElement('meta');
+            Object.keys(tagDef).forEach(key => {
+                tag.setAttribute(key, tagDef[key]);
+            });
+            tag.setAttribute('data-vue-router-controlled', '');
+            return tag;
+        })
+        .forEach(tag => document.head.appendChild(tag));
     next();
 });
 router.afterEach((to, from) => {
--- src/core/routes.d.ts
+++ src/core/routes.d.ts
@@ -2,4 +2,11 @@ type Routes = {
     path: string;
     name: string;
     component: any;
+    meta?: {
+        title?: string;
+        metaTags?: {
+            name: string;
+            content: string;
+        }[];
+    };
 }[];
