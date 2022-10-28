298ca2a7df54b5a4665bae6ba2a84420578d604a
Gifaldy Azka <me@falcxxdev.ml>
2022-10-29 00:41:47 +0700

    feat: env value checking

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,9 @@ import './types/vue.d';
 import App from './App.vue';
 import router from './core/router';
 
+if (!process.env.SPOTIFY_BEARER_TOKEN) {
+    console.error('Spotify bearer token are required!');
+    process.exit(1);
+}
+
 createApp(App).use(router).mount('#app');
