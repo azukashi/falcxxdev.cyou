4fb595b024298fc5e34d6ea11bffe18fb1c969af
Gifaldy Azka <falcxxdev@pm.me>
2023-07-02 21:48:51 +0700

    refactor(content): migrate & fetch content from sanity

--- components/Timeline.vue
+++ components/Timeline.vue
@@ -1,48 +1,11 @@
 <script setup lang="ts">
-type Timeline = {
-    latest?: boolean | undefined;
-    icon: string;
-    title: string;
-    date: string;
-    description: string;
-}[];
-
-const timelines: Timeline = [
-    {
-        latest: true,
-        icon: 'material-symbols:deployed-code',
-        title: 'Website UI v2.0.0',
-        date: 'April 29th, 2023',
-        description: 'Stable release from previous release for this website.',
-    },
-    {
-        icon: 'mdi:sony-playstation',
-        title: 'First Livestream on YouTube',
-        date: 'April 29th, 2023',
-        description: 'Starting first Livestreaming journey on YouTube using PS4.',
-    },
-    {
-        icon: 'mdi:source-branch-plus',
-        title: 'Website UI v2.0.0-beta.1',
-        date: 'April 18th, 2023',
-        description:
-            "This website is just redesigned into new looking user interface. This design system is inspired from Vercel's Geist UI.",
-    },
-    {
-        latest: undefined,
-        icon: 'mdi:wrench',
-        title: "Being moderator at Choco Latte's community",
-        date: 'February 15th, 2023',
-        description:
-            'Being trusted by Choco Latte herself for being moderator on her YouTube channel and Discord server.',
-    },
-];
+const { data }: any = await useFetch('/api/sanity?schema=about');
 </script>
 
 <template>
     <div class="p-4">
         <ol class="relative border-l border-gray-700" v-motion-slide-visible-once-bottom>
-            <li class="mb-10 ml-6" v-for="tl in timelines">
+            <li class="mb-10 ml-6" v-for="tl in data.timeline">
                 <span
                     class="absolute flex items-center justify-center w-6 h-6 rounded-full -left-3 ring-8 ring-white ring-opacity-[0.3] bg-white bg-opacity-[0.5]"
                 >
