a49e8f01b994340c0d1a9d5ebf1aae008ad90e6b
Gifaldy Azka <falcxxdev@pm.me>
2023-07-02 21:44:14 +0700

    chore(config): define configuration for nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', '@vueuse/motion/nuxt', 'nuxt-icon'],
+    modules: ['@nuxt/image', '@nuxtjs/tailwindcss', '@nuxtjs/sanity', '@vueuse/motion/nuxt', 'nuxt-icon'],
     builder: 'vite',
     app: {
         pageTransition: {
@@ -8,8 +8,22 @@ export default defineNuxtConfig({
             mode: 'out-in',
         },
     },
+    image: {
+        domains: ['cdn.upload.systems', 'cdn.sanity.io'],
+        format: ['webp'],
+        alias: {
+            uploadsys: 'https://cdn.upload.systems/uploads',
+        },
+        sanity: {
+            projectId: 'sxpz540u',
+        },
+    },
     tailwindcss: {
         cssPath: '~/assets/css/tailwind.css',
+    },
+    sanity: {
+        projectId: 'sxpz540u',
+        minimal: true,
     },
     imports: {
         dirs: ['./composables', './components', './scripts', './utils'],
