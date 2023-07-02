daccd328671bb28349db720ee631cdd9b4668755
Gifaldy Azka <falcxxdev@pm.me>
2023-07-02 21:50:15 +0700

    feat(component): add sanity content debugger

---
+++ components/Debugger.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps({
+    rootData: {
+        required: true,
+        type: Object,
+    },
+});
+</script>
+
+<template>
+    <div>
+        <h3 class="font-semibold my-2">Sanity.io content debug</h3>
+        <code class="text-pink-500">{{ rootData }}</code>
+    </div>
+</template>
+
+<style scoped></style>
