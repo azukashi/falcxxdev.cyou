aae9870b33d9a40f346bbc4e4dc882c7424f894d
Gifaldy Azka <falcxxdev@pm.me>
2023-07-02 21:48:03 +0700

    refactor(img): serve image using <nuxt-img> instead of <img>

--- pages/hobbies/index.vue
+++ pages/hobbies/index.vue
@@ -1,23 +1,5 @@
 <script setup lang="ts">
-type AlbumCover = {
-    name: string;
-    image: string;
-}[];
-
-const albums: AlbumCover = [
-    {
-        name: 'Street',
-        image: 'https://cdn.upload.systems/uploads/7xb9V1hM.jpg',
-    },
-    {
-        name: 'Portrait',
-        image: 'https://cdn.upload.systems/uploads/qiuJxmZR.jpg',
-    },
-    {
-        name: 'Life & Nature',
-        image: 'https://cdn.upload.systems/uploads/vQ12BudG.jpg',
-    },
-];
+const { data }: any = await useFetch('/api/sanity?schema=albums&pure=true');
 </script>
 
 <template>
@@ -30,23 +12,34 @@ const albums: AlbumCover = [
                 <br />
                 Here's what I've been taking on.
                 <br />
-                <span class="text-sm font-mono font-medium uppercase">* Album will be updated every time.</span>
+                <code class="text-sm text-pink-500 font-bold uppercase">* Album will be updated every time.</code>
             </p>
             <hr class="mt-5 opacity-[0.06] border-b-[1px]" />
         </div>
         <div class="mt-10 grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            <div class="hobbies__card" v-for="album in albums" v-motion-slide-visible-once-left>
-                <img class="w-auto h-auto" :src="album.image" alt="Album cover" />
-                <div class="ps-4 pe-4 py-2">
-                    <p class="text-2xl font-semibold text-white">{{ album.name }}</p>
-                    <hr class="mt-2 opacity-[0.06]" />
-                    <p class="mt-2 text-base text-[#8F9094]">
-                        <span v-if="album.image.length > 1">{{ album.image.length }} photos</span>
-                        <span v-if="album.image.length < 2">{{ album.image.length }} photo</span>
-                        <span v-if="album.image.length == 0">No photo</span>
-                    </p>
-                    <code class="mt-1 text-sm text-[#3CCF91] font-bold">Last updated: June 30, 2023</code>
-                </div>
+            <div class="hobbies__card cursor-zoom-in" v-for="album in data" v-motion-slide-visible-once-left>
+                <NuxtLink class="cursor-zoom-in" :to="`hobbies/${album.name.toString().toLowerCase()}`">
+                    <nuxt-img
+                        format="webp"
+                        width="1920"
+                        height="1280"
+                        quality="50"
+                        class="w-auto h-auto"
+                        :src="album.cover"
+                        alt="Album cover"
+                        loading="lazy"
+                    />
+                    <div class="ps-4 pe-4 py-2">
+                        <p class="text-2xl font-semibold text-white">{{ album.name }}</p>
+                        <!-- <hr class="mt-2 opacity-[0.06]" />
+                        <p class="mt-2 text-base text-[#8F9094]">
+                            <span v-if="album.cover.length > 1">{{ album.cover.length }} pictures</span>
+                            <span v-if="album.cover.length < 2">{{ album.cover.length }} picture</span>
+                            <span v-if="album.cover.length == 0">No picture</span>
+                        </p>
+                        <code class="mt-1 text-sm text-[#3CCF91] font-bold">Last updated: June 30, 2023</code> -->
+                    </div>
+                </NuxtLink>
             </div>
         </div>
     </div>
