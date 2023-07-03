9df6221987ed515aa39ff236efb61d52b960c25b
Gifaldy Azka <falcxxdev@pm.me>
2023-07-03 21:03:36 +0700

    feat(script): create _useBlobity() function

---
+++ scripts/useBlobity.ts
@@ -0,0 +1,10 @@
+import useBlobity from 'blobity/lib/vue/useBlobity';
+
+export const _useBlobity = () => {
+    useBlobity({
+        invert: true,
+        dotColor: '#3CCF91',
+        font: 'Lexend',
+        fontSize: 13,
+    });
+};
